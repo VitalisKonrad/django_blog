32a6fac4d53c6ad09b883b5422e996a44dcc6dd3
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-02-14 16:36:47 +0300

    new part

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -8,3 +8,8 @@ register = template.Library()
 def total_posts():
     #return Post.published.name
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_psts(count=3):
+    latest_post = Post.published.order_by('-publish')[:count]
+    return {'latest_post':latest_post}
