31ed75445e0fb614643e9a484aee4c1465d5fb66
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-02-18 18:03:54 +0300

    new part

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,11 +5,15 @@ from  django.db.models import Count
 register = template.Library()
 
 @register.simple_tag()
+def get_most_commented_posts(count=2):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+@register.simple_tag()
 def total_posts():
     #return Post.published.name
     return Post.published.count()
 
-@register.inclusion_tag('blog/post/latest_post.html')
-def show_latest_psts(count=3):
-    latest_post = Post.published.order_by('-publish')[:count]
-    return {'latest_post':latest_post}
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=4):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts':latest_posts}
